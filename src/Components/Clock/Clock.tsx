46081d69a11430d4d9ab15ce3b3594de72eb49d0
Vitalick <miraigeweb@gmail.com>
2023-01-12 10:51:51 +0300

    made a component digit clock and analog clock

--- src/Components/Clock/Clock.tsx
+++ src/Components/Clock/Clock.tsx
@@ -1,27 +1,85 @@
 import React, {useEffect, useState} from 'react';
+import analogClock from './image/clock-seiko.png';
+import st from './Clock.module.css'
+
 
 const addZero = (num: number) => num < 10 ? '0' + num : num
 
+//если эта компонента умрет, и уберется с экрана, то сайдэффект останется из-за функции setInterval
+//так как асинхронность, которая поражадается браузером продолжает работать и происходит утечка памяти
+
 export const Clock = () => {
-    const [date, setDate] = useState(new Date())
+    const [clockAnalog, setClockAnalog] = useState<Date>(new Date())
+    const [clockDigit, setClockDigit] = useState<Date>(new Date())
+    const [changeMode, setChangeMode] = useState<boolean>(false)
+    const [changeTitle, setChangeTitle] = useState<string>('Digit mode')
+
+    const clickChangeMode = () => {
+        if (!changeMode) {
+            setChangeMode(!changeMode)
+            setChangeTitle('Analog mode')
+        } else {
+            setChangeMode(false)
+            setChangeTitle('Digit mode')
+        }
+    }
+
+    const deg = 6
+    let arrowHour = {
+        transform: `rotate(${clockAnalog.getHours() * 30}deg)`
+    };
+    let arrowMinute = {
+        transform: `rotate(${clockAnalog.getMinutes() * deg}deg)`
+    };
+    let arrowSecond =  {
+        transform: `rotate(${clockAnalog.getSeconds() * deg}deg)`
+    }
 
     useEffect(() => {
         const intervalID = setInterval(() => {
-            setDate(new Date())
+            setClockAnalog(new Date())
+        }, 1000)
+
+        const intervalID2 = setInterval(() => {
+            setClockDigit(new Date())
         }, 1000)
 
         return () => {
             clearInterval(intervalID)
+            clearInterval(intervalID2)
         }
     }, [])
 
+
     return (
-        <div>
-            <span>{addZero(date.getHours())}</span>
-            :
-            <span>{addZero(date.getMinutes())}</span>
-            :
-            <span>{addZero(date.getSeconds())}</span>
+        <div className={st.clock}>
+            {changeMode !== true
+            ? <div className={st.analogClock}>
+                <img src={analogClock} alt=""/>
+                <div className={st.hour}>
+                    <div className={st.hours} style={arrowHour}></div>
+                </div>
+                <div className={st.minute}>
+                    <div className={st.minutes} style={arrowMinute}></div>
+                </div>
+                <div className={st.second}>
+                    <div className={st.seconds} style={arrowSecond}></div>
+                    <div style={arrowSecond}></div>
+                </div>
+            </div>
+
+            : <div className={st.digitalClockOutside}>
+                <div className={st.digitalClockInside}>
+                    <span className={st.digit}>{addZero(clockDigit.getHours())}</span>
+                    <span className={st.twoDotes}>:</span>
+                    <span className={st.digit}>{addZero(clockDigit.getMinutes())}</span>
+                    <span className={st.twoDotes}>:</span>
+                    <span className={st.digit}>{addZero(clockDigit.getSeconds())}</span>
+                </div>
+            </div>}
+            <div className={st.buttonMode}>
+                <button className={st.mode} onClick={clickChangeMode}>{changeTitle}</button>
+            </div>
         </div>
     );
 };
